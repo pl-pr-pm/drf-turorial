44089cd9f2de53361e167a5d9bc4995fced08920
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-20 11:35:50 +0900

    スニペットの取得・変更のAPIを作成

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,30 +2,19 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
-  # シリアル/デシリアル化 されるフィールドを定義
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
 
-    # serializer.save() 実行時にインスタンスの作成・変更で参照されるメソッド
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+# from snippets.serializers import SnippetSerializer
+# serializer = SnippetSerializer()
+# print(repr(serializer))
+# # SnippetSerializer():
+# #    id = IntegerField(label='ID', read_only=True)
+# #    title = CharField(allow_blank=True, max_length=100, required=False)
+# #    code = CharField(style={'base_template': 'textarea.html'})
+# #    linenos = BooleanField(required=False)
+# #    language = ChoiceField(choices=[('Clipper', 'FoxPro'), ('Cucumber', 'Gherkin'), ('RobotFramework', 'RobotFramework'), ('abap', 'ABAP'), ('ada', 'Ada')...
+# #    style = ChoiceField(choices=[('autumn', 'autumn'), ('borland', 'borland'), ('bw', 'bw'), ('colorful', 'colorful')...
